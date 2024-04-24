184f271c63eed4b0bba34f14b5cca97323715045
riasad-alvi <alvi101.cs@gmail.com>
2024-04-24 14:05:50 +0600

    P1

--- 73. Set Matrix Zeroes.cpp
+++ 73. Set Matrix Zeroes.cpp
@@ -11,7 +11,16 @@ using namespace std;
 
 using ll = long long int;
 
-void mark_row(int i, vector<vector<int>> &matrix, int m)
+/*  BRUTE :
+
+    T : 0(n ^ 3)
+    S : 0(1)
+
+*/
+
+//__________________________________________________________
+
+/* void mark_row(int i, vector<vector<int>> &matrix, int m)
 {
 
     for (int j = 0; j < m; j++)
@@ -38,7 +47,7 @@ public:
     {
         ll n = matrix.size();
         ll m = matrix[0].size();
-        bool flag = false;
+        bool any0 = false;
 
         for (int i = 0; i < n; i++)
         {
@@ -48,12 +57,12 @@ public:
                 {
                     mark_row(i, matrix, m);
                     mark_col(j, matrix, n);
-                    flag = true;
+                    any0 = true;
                 }
             }
         }
 
-        if (flag)
+        if (any0) // if the matrix doesnot contain 0 , no need to modify
         {
             for (int i = 0; i < n; i++)
             {
@@ -67,7 +76,7 @@ public:
             }
         }
 
-        // print if there was any of 0 :
+        // print :
 
         for (int i = 0; i < n; i++)
         {
@@ -81,6 +90,156 @@ public:
         return;
     }
 };
+*/
+
+/*
+
+
+
+*/
+
+/*  BETTER :
+
+    T : 0(n ^ 3)
+    S : 0(1)
+
+*/
+
+//__________________________________________________________
+
+/*class Solution
+{
+public:
+    void setZeroes(vector<vector<int>> &matrix)
+    {
+        int n = matrix.size();
+        int m = matrix[0].size();
+
+        vector<int> mark_row(n, 0); //to keep record of marked rows
+        vector<int> mark_col(m, 0);
+
+                for (int i = 0; i < n; i++)
+        {
+            for (int j = 0; j < m; j++)
+            {
+                if (matrix[i][j] == 0)
+                {
+                    mark_row[i] = 1;
+                    mark_col[j] = 1;
+                }
+            }
+        }
+
+        for (int i = 0; i < n; i++)
+        {
+            for (int j = 0; j < m; j++)
+            {
+                if (mark_row[i] == 1 || mark_col[j] == 1)
+                {
+                    matrix[i][j] = 0;
+                }
+            }
+        }
+
+        // print :
+
+        for (int i = 0; i < n; i++)
+        {
+            for (int j = 0; j < m; j++)
+            {
+                cout << matrix[i][j] << ",";
+            }
+            cout << "\n";
+        }
+
+        return;
+    }
+};*/
+
+/*  OPTIMAL  :
+
+    T : 0(n ^ 2)
+    S : NA
+*/
+//__________________________________________________________
+class Solution
+{
+public:
+    void setZeroes(vector<vector<int>> &matrix)
+    {
+        int n = matrix.size();
+        int m = matrix[0].size();
+
+        int col0 = 1; // as matrix[0][0] is a common part so take col0 separately
+
+        for (int i = 0; i < n; i++)
+        {
+            for (int j = 0; j < m; j++)
+            {
+                if (matrix[i][j] == 0)
+                {
+                    matrix[i][0] = 0; // mark_row
+
+                    if (j != 0)
+                    {
+                        matrix[0][j] = 0; // mark_col
+                    }
+                    else if (j == 0) // first col
+                    {
+                        col0 = 0;
+                    }
+                }
+            }
+        }
+
+        // without marked_row and marked_col part :
+
+        for (int i = 1; i < n; i++)
+        {
+            for (int j = 1; j < m; j++)
+            {
+                if (matrix[i][j] != 0)
+                {
+                    if (matrix[i][0] == 0 || matrix[0][j] == 0)
+                    {
+                        matrix[i][j] = 0;
+                    }
+                }
+            }
+        }
+
+        // marked col part:
+
+        for (int j = 1; j < m; j++)
+        {
+            if (matrix[0][j] == 0 || matrix[0][0] == 0)
+                matrix[0][j] = 0;
+        }
+
+        // marked row part :
+
+        for (int i = 0; i < n; i++)
+        {
+            if (col0 == 0 || matrix[i][0] == 0)
+            {
+                matrix[i][0] = 0;
+            }
+        }
+
+        // print :
+
+        for (int i = 0; i < n; i++)
+        {
+            for (int j = 0; j < m; j++)
+            {
+                cout << matrix[i][j] << ",";
+            }
+            cout << "\n";
+        }
+    }
+}
+
+;
 
 // int32_t main()
 // {
